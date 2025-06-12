d37285b935cb51cf1e9fe4331a328b5986d62d54
Adriel Avila <adrielavila@MacBook-Pro-de-Adriel.local>
2025-06-12 18:42:39 -0500

    feat: agregar nuevas funciones de tap y manejo de errores en TaskEither, mejorando la flexibilidad y el control de flujo

--- lib/reader-task-either.ts
+++ lib/reader-task-either.ts
@@ -13,235 +13,296 @@ export type URI = typeof URI;
 export type ReaderTaskEither<R, E, A> = R.Reader<R, TE.TaskEither<E, A>>;
 
 declare module "./URIS3" {
-  interface URItoKind3<_R, _E, _A> {
-    ReaderTaskEither: ReaderTaskEither<_R, _E, _A>;
-  }
+	interface URItoKind3<_R, _E, _A> {
+		ReaderTaskEither: ReaderTaskEither<_R, _E, _A>;
+	}
 }
 
 export const of =
-  <R, E, A>(a: A): ReaderTaskEither<R, E, A> =>
-  () =>
-    TE.of(a);
+	<R, E, A>(a: A): ReaderTaskEither<R, E, A> =>
+	() =>
+		TE.of(a);
 
 export const from =
-  <R, E, A>(te: TE.TaskEither<E, A>): ReaderTaskEither<R, E, A> =>
-  () =>
-    te;
+	<R, E, A>(te: TE.TaskEither<E, A>): ReaderTaskEither<R, E, A> =>
+	() =>
+		te;
 
 export const fromReader =
-  <R, A>(r: R.Reader<R, A>): ReaderTaskEither<R, never, A> =>
-  (ctx: R) =>
-    TE.of(r(ctx));
+	<R, A>(r: R.Reader<R, A>): ReaderTaskEither<R, never, A> =>
+	(ctx: R) =>
+		TE.of(r(ctx));
 
 export const ask =
-  <R>(): ReaderTaskEither<R, never, R> =>
-  (ctx: R) =>
-    TE.of(ctx);
+	<R>(): ReaderTaskEither<R, never, R> =>
+	(ctx: R) =>
+		TE.of(ctx);
 
 export const tap =
-  <R, E, A>(fn: Fn<A, void>) =>
-  (rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> =>
-  (ctx: R) =>
-    pipe(rte(ctx), TE.tap(fn));
+	<R, E, A>(fn: Fn<A, void>) =>
+	(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> =>
+	(ctx: R) =>
+		pipe(rte(ctx), TE.tap(fn));
 
 export const tapR =
-  <R, A>(fn: Fn<R, Fn<A, void>>) =>
-  <E>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> => {
-    return (ctx: R) => pipe(rte(ctx), TE.tap(fn(ctx)));
-  };
+	<R, A>(fn: Fn<R, Fn<A, void>>) =>
+	<E>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> => {
+		return (ctx: R) => pipe(rte(ctx), TE.tap(fn(ctx)));
+	};
 
 export const tapLeft =
-  <R, E, A>(fn: Fn<E, void>) =>
-  (rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> =>
-  (ctx: R) =>
-    pipe(rte(ctx), TE.tapLeft(fn));
+	<R, E, A>(fn: Fn<E, void>) =>
+	(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> =>
+	(ctx: R) =>
+		pipe(rte(ctx), TE.tapLeft(fn));
 
 export const tapRTE =
-  <R2, E2, A>(fn: Fn<A, ReaderTaskEither<R2, E2, void>>) =>
-  <R1, E1>(rte: ReaderTaskEither<R1, E1, A>): ReaderTaskEither<R1 & R2, E1 | E2, A> => {
-    return (ctx: R1 & R2) =>
-      pipe(
-        rte(ctx),
-        TE.tapTE((a) => fn(a)(ctx))
-      );
-  };
+	<R2, E2, A>(fn: Fn<A, ReaderTaskEither<R2, E2, void>>) =>
+	<R1, E1>(rte: ReaderTaskEither<R1, E1, A>): ReaderTaskEither<R1 & R2, E1 | E2, A> => {
+		return (ctx: R1 & R2) =>
+			pipe(
+				rte(ctx),
+				TE.tapTE((a) => fn(a)(ctx)),
+			);
+	};
 
 export const tapLeftRTE =
-  <R2, E1, E2, A>(fn: Fn<E1, ReaderTaskEither<R2, E2, void>>) =>
-  <R1>(rte: ReaderTaskEither<R1, E1, A>): ReaderTaskEither<R1 & R2, E1 | E2, A> => {
-    return (ctx: R1 & R2) =>
-      pipe(
-        rte(ctx),
-        TE.tapLeftTE((e) => fn(e)(ctx))
-      );
-  };
+	<R2, E1, E2, A>(fn: Fn<E1, ReaderTaskEither<R2, E2, void>>) =>
+	<R1>(rte: ReaderTaskEither<R1, E1, A>): ReaderTaskEither<R1 & R2, E1 | E2, A> => {
+		return (ctx: R1 & R2) =>
+			pipe(
+				rte(ctx),
+				TE.tapLeftTE((e) => fn(e)(ctx)),
+			);
+	};
+
+export const tapRTEAsync =
+	<R2, E2, A>(fn: Fn<A, ReaderTaskEither<R2, E2, void>>) =>
+	<R1, E1>(rte: ReaderTaskEither<R1, E1, A>): ReaderTaskEither<R1 & R2, E1, A> => {
+		return (ctx: R1 & R2) =>
+			pipe(
+				rte(ctx),
+				TE.tapTEAsync((a) => fn(a)(ctx)),
+			);
+	};
+
+export const tapRTEDetached =
+	<R2, E2, A>(
+		fn: Fn<A, ReaderTaskEither<R2, E2, void>>,
+		onError?: Fn<E2, void>
+	) =>
+	<R1, E1>(rte: ReaderTaskEither<R1, E1, A>): ReaderTaskEither<R1 & R2, E1, A> => {
+		return (ctx: R1 & R2) =>
+			pipe(
+				rte(ctx),
+				TE.tapTEDetached((a) => fn(a)(ctx), onError),
+			);
+	};
+
+export const tapLeftRTEAsync =
+	<R2, E1, E2, A>(fn: Fn<E1, ReaderTaskEither<R2, E2, void>>) =>
+	<R1>(rte: ReaderTaskEither<R1, E1, A>): ReaderTaskEither<R1 & R2, E1, A> => {
+		return (ctx: R1 & R2) =>
+			pipe(
+				rte(ctx),
+				TE.tapLeftTEAsync((e) => fn(e)(ctx)),
+			);
+	};
+
+export const tapLeftRTEDetached =
+	<R2, E1, E2, A>(
+		fn: Fn<E1, ReaderTaskEither<R2, E2, void>>,
+		onError?: Fn<E2, void>
+	) =>
+	<R1>(rte: ReaderTaskEither<R1, E1, A>): ReaderTaskEither<R1 & R2, E1, A> => {
+		return (ctx: R1 & R2) =>
+			pipe(
+				rte(ctx),
+				TE.tapLeftTEDetached((e) => fn(e)(ctx), onError),
+			);
+	};
 
 export const map =
-  <A, B>(fn: Fn<A, B>) =>
-  <R, E>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, B> => {
-    return (ctx: R) => pipe(rte(ctx), TE.map(fn));
-  };
+	<A, B>(fn: Fn<A, B>) =>
+	<R, E>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, B> => {
+		return (ctx: R) => pipe(rte(ctx), TE.map(fn));
+	};
 
 export const mapWhithContext =
-  <R, A, B>(fn: Fn<R, Fn<A, B>>) =>
-  <E>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, B> => {
-    return (ctx: R) => pipe(rte(ctx), TE.map(fn(ctx)));
-  };
+	<R, A, B>(fn: Fn<R, Fn<A, B>>) =>
+	<E>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, B> => {
+		return (ctx: R) => pipe(rte(ctx), TE.map(fn(ctx)));
+	};
 
 export const mapLeft =
-  <E, E2>(fn: Fn<E, E2>) =>
-  <R, A>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E2, A> => {
-    return (ctx: R) => pipe(rte(ctx), TE.mapLeft(fn));
-  };
+	<E, E2>(fn: Fn<E, E2>) =>
+	<R, A>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E2, A> => {
+		return (ctx: R) => pipe(rte(ctx), TE.mapLeft(fn));
+	};
 
 export const join = <R, E, A>(
-  rte: ReaderTaskEither<R, E, ReaderTaskEither<R, E, A>>
+	rte: ReaderTaskEither<R, E, ReaderTaskEither<R, E, A>>,
 ): ReaderTaskEither<R, E, A> => {
-  return (ctx: R) =>
-    pipe(
-      rte(ctx),
-      TE.chain((rte) => rte(ctx))
-    );
+	return (ctx: R) =>
+		pipe(
+			rte(ctx),
+			TE.chain((rte) => rte(ctx)),
+		);
 };
 
 export const chain =
-  <R, E, A, B>(fn: Fn<A, ReaderTaskEither<R, E, B>>) =>
-  (rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, B> => {
-    return (ctx: R) =>
-      pipe(
-        rte(ctx),
-        TE.chain((a) => fn(a)(ctx))
-      );
-  };
+	<R, E, A, B>(fn: Fn<A, ReaderTaskEither<R, E, B>>) =>
+	(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, B> => {
+		return (ctx: R) =>
+			pipe(
+				rte(ctx),
+				TE.chain((a) => fn(a)(ctx)),
+			);
+	};
 
 export const chainLeft =
-  <R, E, A>(fn: Fn<E, ReaderTaskEither<R, E, A>>) =>
-  (rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> => {
-    return (ctx: R) =>
-      pipe(
-        rte(ctx),
-        TE.chainLeft((e) => fn(e)(ctx))
-      );
-  };
+	<R, E, A>(fn: Fn<E, ReaderTaskEither<R, E, A>>) =>
+	(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> => {
+		return (ctx: R) =>
+			pipe(
+				rte(ctx),
+				TE.chainLeft((e) => fn(e)(ctx)),
+			);
+	};
 
 export const chaint =
-  <E, A, B>(fn: Fn<A, TE.TaskEither<E, B>>) =>
-  <R>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, B> => {
-    return (ctx: R) => pipe(rte(ctx), TE.chain(fn));
-  };
+	<E, A, B>(fn: Fn<A, TE.TaskEither<E, B>>) =>
+	<R>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, B> => {
+		return (ctx: R) => pipe(rte(ctx), TE.chain(fn));
+	};
 
 export const chainw =
-  <R, E2, A, B>(fn: Fn<A, ReaderTaskEither<R, E2, B>>) =>
-  <E1>(rte: ReaderTaskEither<R, E1, A>): ReaderTaskEither<R, E1 | E2, B> => {
-    return (ctx: R) =>
-      pipe(
-        rte(ctx),
-        TE.chainw((a) => fn(a)(ctx))
-      );
-  };
+	<R, E2, A, B>(fn: Fn<A, ReaderTaskEither<R, E2, B>>) =>
+	<E1>(rte: ReaderTaskEither<R, E1, A>): ReaderTaskEither<R, E1 | E2, B> => {
+		return (ctx: R) =>
+			pipe(
+				rte(ctx),
+				TE.chainw((a) => fn(a)(ctx)),
+			);
+	};
 
 export const chainwc =
-  <R2, E, A, B>(fn: Fn<A, ReaderTaskEither<R2, E, B>>) =>
-  <R1>(rte: ReaderTaskEither<R1, E, A>): ReaderTaskEither<R1 & R2, E, B> => {
-    return (ctx: R1 & R2) =>
-      pipe(
-        rte(ctx),
-        TE.chain((a) => fn(a)(ctx))
-      );
-  };
+	<R2, E, A, B>(fn: Fn<A, ReaderTaskEither<R2, E, B>>) =>
+	<R1>(rte: ReaderTaskEither<R1, E, A>): ReaderTaskEither<R1 & R2, E, B> => {
+		return (ctx: R1 & R2) =>
+			pipe(
+				rte(ctx),
+				TE.chain((a) => fn(a)(ctx)),
+			);
+	};
 
 export const provide =
-  <R extends Record<any, any>, R2 extends Partial<R>>(r2: R2) =>
-  <E, A>(ra: ReaderTaskEither<R, E, A>): ReaderTaskEither<Omit<R, keyof R2>, E, A> =>
-  (r: Omit<R, keyof R2>) =>
-    ra({ ...r2, ...r } as unknown as R);
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	<R extends Record<any, any>, R2 extends Partial<R>>(r2: R2) =>
+	<E, A>(ra: ReaderTaskEither<R, E, A>): ReaderTaskEither<Omit<R, keyof R2>, E, A> =>
+	(r: Omit<R, keyof R2>) =>
+		ra({ ...r2, ...r } as unknown as R);
 
 export const fold =
-  <R, E, A, B>(onLeft: Fn<E, B>, onRight: Fn<A, B>) =>
-  (rte: ReaderTaskEither<R, E, A>): R.Reader<R, Task<B>> => {
-    return (ctx: R) => pipe(rte(ctx), TE.fold(onLeft, onRight));
-  };
+	<R, E, A, B>(onLeft: Fn<E, B>, onRight: Fn<A, B>) =>
+	(rte: ReaderTaskEither<R, E, A>): R.Reader<R, Task<B>> => {
+		return (ctx: R) => pipe(rte(ctx), TE.fold(onLeft, onRight));
+	};
 
 export const run =
-  <R>(r: R) =>
-  <E, A>(rte: ReaderTaskEither<R, E, A>): TE.TaskEither<E, A> =>
-    rte(r);
+	<R>(r: R) =>
+	<E, A>(rte: ReaderTaskEither<R, E, A>): TE.TaskEither<E, A> =>
+		rte(r);
 
 export const apply =
-  <R2, E, A, B>(rtefn: ReaderTaskEither<R2, E, Fn<A, B>>) =>
-  <R1>(rte: ReaderTaskEither<R1, E, A>): ReaderTaskEither<R1 & R2, E, B> => {
-    return (ctx: R1 & R2) => pipe(rte(ctx), TE.apply(rtefn(ctx)));
-  };
+	<R2, E, A, B>(rtefn: ReaderTaskEither<R2, E, Fn<A, B>>) =>
+	<R1>(rte: ReaderTaskEither<R1, E, A>): ReaderTaskEither<R1 & R2, E, B> => {
+		return (ctx: R1 & R2) => pipe(rte(ctx), TE.apply(rtefn(ctx)));
+	};
 
-// Helper types for the new sequenceObject and sequence
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 type ReaderContexts<T extends ReaderTaskEither<any, any, any>[]> = UnionToIntersection<
-  {
-    [K in keyof T]: T[K] extends ReaderTaskEither<infer R, any, any> ? R : never;
-  }[keyof T & number]
+	{
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		[K in keyof T]: T[K] extends ReaderTaskEither<infer R, any, any> ? R : never;
+	}[keyof T & number]
 >;
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void
-  ? I
-  : never;
+	? I
+	: never;
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 type ReaderErrors<T extends ReaderTaskEither<any, any, any>[]> = {
-  [K in keyof T & number]: T[K] extends ReaderTaskEither<any, infer E, any> ? E : never;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	[K in keyof T & number]: T[K] extends ReaderTaskEither<any, infer E, any> ? E : never;
 }[keyof T & number];
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 type ReaderValues<T extends ReaderTaskEither<any, any, any>[]> = {
-  [K in keyof T & number]: T[K] extends ReaderTaskEither<any, any, infer A> ? A : never;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	[K in keyof T & number]: T[K] extends ReaderTaskEither<any, any, infer A> ? A : never;
 };
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const sequence = <T extends ReaderTaskEither<any, any, any>[]>(
-  arr: T
+	arr: T,
 ): ReaderTaskEither<ReaderContexts<T>, ReaderErrors<T>, ReaderValues<T>> => {
-  return (ctx: ReaderContexts<T>) => {
-    const taskEitherArr = arr.map((rte) => rte(ctx));
-    return pipe(taskEitherArr, TE.sequence) as TE.TaskEither<ReaderErrors<T>, ReaderValues<T>>;
-  };
+	return (ctx: ReaderContexts<T>) => {
+		const taskEitherArr = arr.map((rte) => rte(ctx));
+		return pipe(taskEitherArr, TE.sequence) as TE.TaskEither<ReaderErrors<T>, ReaderValues<T>>;
+	};
 };
 
 // Helper types for the new sequenceObject
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 type ReaderObjectContexts<T extends Record<string, ReaderTaskEither<any, any, any>>> =
-  UnionToIntersection<
-    {
-      [K in keyof T]: T[K] extends ReaderTaskEither<infer R, any, any> ? R : never;
-    }[keyof T]
-  >;
+	UnionToIntersection<
+		{
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			[K in keyof T]: T[K] extends ReaderTaskEither<infer R, any, any> ? R : never;
+		}[keyof T]
+	>;
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 type ReaderObjectErrors<T extends Record<string, ReaderTaskEither<any, any, any>>> = {
-  [K in keyof T]: T[K] extends ReaderTaskEither<any, infer E, any> ? E : never;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	[K in keyof T]: T[K] extends ReaderTaskEither<any, infer E, any> ? E : never;
 }[keyof T];
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 type ReaderObjectValues<T extends Record<string, ReaderTaskEither<any, any, any>>> = {
-  [K in keyof T]: T[K] extends ReaderTaskEither<any, any, infer A> ? A : never;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	[K in keyof T]: T[K] extends ReaderTaskEither<any, any, infer A> ? A : never;
 };
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const sequenceObject = <T extends Record<string, ReaderTaskEither<any, any, any>>>(
-  obj: T
+	obj: T,
 ): ReaderTaskEither<ReaderObjectContexts<T>, ReaderObjectErrors<T>, ReaderObjectValues<T>> => {
-  return (ctx: ReaderObjectContexts<T>) => {
-    return pipe(
-      obj,
-      O.mapValues((rte) => rte(ctx)),
-      TE.sequenceObject,
-      (d) => d as TE.TaskEither<ReaderObjectErrors<T>, ReaderObjectValues<T>>
-    );
-  };
+	return (ctx: ReaderObjectContexts<T>) => {
+		return pipe(
+			obj,
+			O.mapValues((rte) => rte(ctx)),
+			TE.sequenceObject,
+			(d) => d as TE.TaskEither<ReaderObjectErrors<T>, ReaderObjectValues<T>>,
+		);
+	};
 };
 
 type Config = { concurrency?: number; delay?: number };
 export const concurrency =
-  (config?: Config) =>
-  <R, E, A>(arr: ReaderTaskEither<R, E, A>[]): ReaderTaskEither<R, E, A[]> => {
-    return (ctx: R) => TE.concurrency(config)(arr.map((rte) => rte(ctx)));
-  };
+	(config?: Config) =>
+	<R, E, A>(arr: ReaderTaskEither<R, E, A>[]): ReaderTaskEither<R, E, A[]> => {
+		return (ctx: R) => TE.concurrency(config)(arr.map((rte) => rte(ctx)));
+	};
 
 export const delay =
-  (ms: number) =>
-  <R, E, A>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> => {
-    return (ctx: R) => pipe(rte(ctx), TE.delay(ms));
-  };
+	(ms: number) =>
+	<R, E, A>(rte: ReaderTaskEither<R, E, A>): ReaderTaskEither<R, E, A> => {
+		return (ctx: R) => pipe(rte(ctx), TE.delay(ms));
+	};
 
 interface RTEF extends Monad3<URI>, Applicative3<URI> {}
 
--- lib/task-either.ts
+++ lib/task-either.ts
@@ -19,42 +19,44 @@ export type URI = typeof URI;
 export type TaskEither<E, T> = Task<Either<E, T>>;
 
 declare module "./URIS2" {
-  interface URItoKind2<E, A> {
-    TaskEither: TaskEither<E, A>;
-  }
+	interface URItoKind2<E, A> {
+		TaskEither: TaskEither<E, A>;
+	}
 }
 
 export const taskify =
-  <Args extends any[], R>(fn: (...args: Args) => Promise<R>) =>
-  (...args: Args): TaskEither<Error, R> =>
-  () =>
-    fn(...args)
-      .then(E.right)
-      .catch((e) => E.left(new Error(String(e)))) as Promise<Either<Error, R>>;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		<Args extends any[], R>(fn: (...args: Args) => Promise<R>) =>
+		(...args: Args): TaskEither<Error, R> =>
+		() =>
+			fn(...args)
+				.then(E.right)
+				.catch((e) => E.left(new Error(String(e)))) as Promise<Either<Error, R>>;
 
 export const taskifyEither =
-  <Args extends any[], E, R>(fn: (...args: Args) => Promise<Either<E, R>>) =>
-  (...args: Args): TaskEither<E, R> =>
-  () =>
-    fn(...args);
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		<Args extends any[], E, R>(fn: (...args: Args) => Promise<Either<E, R>>) =>
+		(...args: Args): TaskEither<E, R> =>
+		() =>
+			fn(...args);
 
 export const tryCatch =
-  <E>(onError: Fn<unknown, E>) =>
-  <A>(task: Task<A>): TaskEither<E, A> =>
-  () => {
-    return T.run(task)
-      .then(E.right)
-      .catch((error) => E.left(onError(error))) as Promise<Either<E, A>>;
-  };
+	<E>(onError: Fn<unknown, E>) =>
+	<A>(task: Task<A>): TaskEither<E, A> =>
+	() => {
+		return T.run(task)
+			.then(E.right)
+			.catch((error) => E.left(onError(error))) as Promise<Either<E, A>>;
+	};
 
 const mapE = (e: unknown) => new Error(String(e));
 
 export const fromPromise = <A>(fn: () => Promise<A>): TaskEither<Error, A> => tryCatch(mapE)(fn);
 
 export const fromEither =
-  <E, A>(either: Either<E, A>): TaskEither<E, A> =>
-  () =>
-    Promise.resolve(either);
+	<E, A>(either: Either<E, A>): TaskEither<E, A> =>
+	() =>
+		Promise.resolve(either);
 
 export const fromTask = <A>(task: Task<A>): TaskEither<Error, A> => tryCatch(mapE)(task);
 
@@ -73,123 +75,175 @@ export const rightTask = <E, A>(ta: Task<A>): TaskEither<E, A> => T.map(E.right)
 export const leftTask = <E, A>(ta: Task<E>): TaskEither<E, A> => T.map(E.left)(ta);
 
 export const tap =
-  <A>(f: Fn<A, void>) =>
-  <E>(as: TaskEither<E, A>) =>
-    pipe(as, T.map(E.tap(f)));
+	<A>(f: Fn<A, void>) =>
+	<E>(as: TaskEither<E, A>) =>
+		pipe(as, T.map(E.tap(f)));
 
 export const tapLeft =
-  <E>(f: Fn<E, void>) =>
-  <A>(as: TaskEither<E, A>) =>
-    pipe(as, T.map(E.tapLeft(f)));
+	<E>(f: Fn<E, void>) =>
+	<A>(as: TaskEither<E, A>) =>
+		pipe(as, T.map(E.tapLeft(f)));
 
 export const tapTE =
-  <E2, A>(f: Fn<A, TaskEither<E2, void>>) =>
-  <E1>(as: TaskEither<E1, A>): TaskEither<E1 | E2, A> => {
-    return pipe(
-      as,
-      T.chain(
-        E.fold(left<E1 | E2, A>, (result) => {
-          return pipe(f(result), T.map(E.fold(E.left<E1 | E2, A>, () => E.right(result))));
-        })
-      )
-    );
-  };
+	<E2, A>(f: Fn<A, TaskEither<E2, void>>) =>
+	<E1>(as: TaskEither<E1, A>): TaskEither<E1 | E2, A> => {
+		return pipe(
+			as,
+			T.chain(
+				E.fold(left<E1 | E2, A>, (result) => {
+					return pipe(f(result), T.map(E.fold(E.left<E1 | E2, A>, () => E.right(result))));
+				}),
+			),
+		);
+	};
+
+export const tapTEAsync =
+	<E2, A>(f: Fn<A, TaskEither<E2, void>>) =>
+	<E1>(as: TaskEither<E1, A>): TaskEither<E1, A> => {
+		return pipe(
+			as,
+			tap((result) => {
+				f(result)().catch(() => void 0);
+			}),
+		);
+	};
+
+export const tapTEDetached =
+	<E2, A>(f: Fn<A, TaskEither<E2, void>>, onError?: Fn<E2, void>) =>
+	<E1>(as: TaskEither<E1, A>): TaskEither<E1, A> => {
+		return pipe(
+			as,
+			tap((result) => {
+				f(result)()
+					.then(() => void 0)
+					.catch((error) => {
+						if (onError) onError(error);
+					});
+			}),
+		);
+	};
 
 export const tapLeftTE =
-  <E1, E2, A>(f: Fn<E1, TaskEither<E2, void>>) =>
-  (as: TaskEither<E1, A>): TaskEither<E1 | E2, A> => {
-    return pipe(
-      as,
-      T.chain(
-        E.fold(
-          (error) => {
-            return pipe(
-              f(error),
-              T.map(
-                E.fold(
-                  () => E.left(error),
-                  () => E.left(error)
-                )
-              )
-            );
-          },
-          (result) => T.of(E.right(result))
-        )
-      )
-    );
-  };
+	<E1, E2, A>(f: Fn<E1, TaskEither<E2, void>>) =>
+	(as: TaskEither<E1, A>): TaskEither<E1 | E2, A> => {
+		return pipe(
+			as,
+			T.chain(
+				E.fold(
+					(error) => {
+						return pipe(
+							f(error),
+							T.map(
+								E.fold(
+									() => E.left(error),
+									() => E.left(error),
+								),
+							),
+						);
+					},
+					(result) => T.of(E.right(result)),
+				),
+			),
+		);
+	};
+
+export const tapLeftTEAsync =
+	<E1, E2, A>(f: Fn<E1, TaskEither<E2, void>>) =>
+	(as: TaskEither<E1, A>): TaskEither<E1, A> => {
+		return pipe(
+			as,
+			tapLeft((error) => {
+				f(error)().catch(() => void 0);
+			}),
+		);
+	};
+
+export const tapLeftTEDetached =
+	<E1, E2, A>(f: Fn<E1, TaskEither<E2, void>>, onError?: Fn<E2, void>) =>
+	(as: TaskEither<E1, A>): TaskEither<E1, A> => {
+		return pipe(
+			as,
+			tapLeft((error) => {
+				f(error)()
+					.then(() => void 0)
+					.catch((err) => {
+						if (onError) onError(err);
+					});
+			}),
+		);
+	};
 
 export const map =
-  <A, B>(f: Fn<A, B>) =>
-  <E>(as: TaskEither<E, A>) =>
-    pipe(as, T.map(E.map(f)));
+	<A, B>(f: Fn<A, B>) =>
+	<E>(as: TaskEither<E, A>) =>
+		pipe(as, T.map(E.map(f)));
 
 export const mapLeft =
-  <E, E2>(f: Fn<E, E2>) =>
-  <A>(as: TaskEither<E, A>) =>
-    pipe(as, T.map(E.mapLeft(f)));
+	<E, E2>(f: Fn<E, E2>) =>
+	<A>(as: TaskEither<E, A>) =>
+		pipe(as, T.map(E.mapLeft(f)));
 
 export const bimap =
-  <E, A, E2, B>(f: Fn<E, E2>, g: Fn<A, B>) =>
-  (as: TaskEither<E, A>) =>
-    pipe(as, T.map(E.bimap(f, g)));
+	<E, A, E2, B>(f: Fn<E, E2>, g: Fn<A, B>) =>
+	(as: TaskEither<E, A>) =>
+		pipe(as, T.map(E.bimap(f, g)));
 
 export const join = <E, A>(tta: TaskEither<E, TaskEither<E, A>>): TaskEither<E, A> => {
-  return () => tta().then(E.fold(compose(P.of, E.left<E, A>), run));
+	return () => tta().then(E.fold(compose(P.of, E.left<E, A>), run));
 };
 
 export const chain =
-  <E, A, B>(f: Fn<A, TaskEither<E, B>>) =>
-  (as: TaskEither<E, A>): TaskEither<E, B> =>
-    pipe(as, T.chain(E.fold(left<E, B>, f)));
+	<E, A, B>(f: Fn<A, TaskEither<E, B>>) =>
+	(as: TaskEither<E, A>): TaskEither<E, B> =>
+		pipe(as, T.chain(E.fold(left<E, B>, f)));
 
 export const tchain =
-  <E, A>(f: Fn<A, TaskEither<E, void>>) =>
-  (as: TaskEither<E, A>): TaskEither<E, A> => {
-    return pipe(
-      as,
-      chain((a) =>
-        pipe(
-          f(a),
-          map(() => a)
-        )
-      )
-    );
-  };
+	<E, A>(f: Fn<A, TaskEither<E, void>>) =>
+	(as: TaskEither<E, A>): TaskEither<E, A> => {
+		return pipe(
+			as,
+			chain((a) =>
+				pipe(
+					f(a),
+					map(() => a),
+				),
+			),
+		);
+	};
 
 export const chainw =
-  <E2, A, B>(f: Fn<A, TaskEither<E2, B>>) =>
-  <E>(as: TaskEither<E, A>): TaskEither<E | E2, B> =>
-    pipe(as, T.chain(E.fold(left<E | E2, B>, f)));
+	<E2, A, B>(f: Fn<A, TaskEither<E2, B>>) =>
+	<E>(as: TaskEither<E, A>): TaskEither<E | E2, B> =>
+		pipe(as, T.chain(E.fold(left<E | E2, B>, f)));
 
 export const chainLeft =
-  <E, A>(f: Fn<E, TaskEither<E, A>>) =>
-  (as: TaskEither<E, A>): TaskEither<E, A> =>
-    pipe(as, T.chain(E.fold(f, right<E, A>)));
+	<E, A>(f: Fn<E, TaskEither<E, A>>) =>
+	(as: TaskEither<E, A>): TaskEither<E, A> =>
+		pipe(as, T.chain(E.fold(f, right<E, A>)));
 
 export const chainLeftw =
-  <E2, E, A>(f: Fn<E, TaskEither<E2, A>>) =>
-  (as: TaskEither<E, A>): TaskEither<E | E2, A> =>
-    pipe(as, T.chain(E.fold(f, right<E | E2, A>)));
+	<E2, E, A>(f: Fn<E, TaskEither<E2, A>>) =>
+	(as: TaskEither<E, A>): TaskEither<E | E2, A> =>
+		pipe(as, T.chain(E.fold(f, right<E | E2, A>)));
 
 export const fold =
-  <E, A, B>(onLeft: Fn<E, B>, onRight: Fn<A, B>) =>
-  (as: TaskEither<E, A>): Task<B> =>
-    pipe(as, T.map(E.fold(onLeft, onRight)));
+	<E, A, B>(onLeft: Fn<E, B>, onRight: Fn<A, B>) =>
+	(as: TaskEither<E, A>): Task<B> =>
+		pipe(as, T.map(E.fold(onLeft, onRight)));
 
 export const orElse = <E, A, E2>(f: Fn<E, TaskEither<E2, A>>) =>
-  compose(T.chain(E.fold(f, right<E2, A>)));
+	compose(T.chain(E.fold(f, right<E2, A>)));
 
 export const getOrElse =
-  <E, A>(f: Fn<E, A>) =>
-  (as: TaskEither<E, A>) =>
-    pipe(as, T.map(E.getOrElse(f)));
+	<E, A>(f: Fn<E, A>) =>
+	(as: TaskEither<E, A>) =>
+		pipe(as, T.map(E.getOrElse(f)));
 
 export const alt =
-  <E, A>(tea: TaskEither<E, A>) =>
-  (tea2: TaskEither<E, A>): TaskEither<E, A> =>
-  () =>
-    tea().then(E.fold(tea2, tea));
+	<E, A>(tea: TaskEither<E, A>) =>
+	(tea2: TaskEither<E, A>): TaskEither<E, A> =>
+	() =>
+		tea().then(E.fold(tea2, tea));
 
 export const toUnion = compose(T.map(E.toUnion));
 
@@ -198,88 +252,88 @@ export const toNullable = compose(T.map(E.toNullable));
 export const toMaybe = compose(T.map(E.toMaybe));
 
 export const toTask = <E, A>(tea: TaskEither<E, A>): Task<A> => {
-  return () =>
-    tea().then(
-      E.fold(
-        (e) => P.reject(e),
-        (a) => P.resolve(a)
-      )
-    );
+	return () =>
+		tea().then(
+			E.fold(
+				(e) => P.reject(e),
+				(a) => P.resolve(a),
+			),
+		);
 };
 
 export const toPromise = <E, A>(tea: TaskEither<E, A>): Promise<A> => {
-  return tea().then(
-    E.fold(
-      (e) => Promise.reject(e),
-      (a) => Promise.resolve(a)
-    )
-  );
+	return tea().then(
+		E.fold(
+			(e) => Promise.reject(e),
+			(a) => Promise.resolve(a),
+		),
+	);
 };
 
 export const apply =
-  <E, A, B>(ap: TaskEither<E, Fn<A, B>>) =>
-  (ta: TaskEither<E, A>): TaskEither<E, B> => {
-    return async () => {
-      const [fn, a] = await Promise.all([ap(), ta()]);
-      return E.apply(fn)(a);
-    };
-  };
+	<E, A, B>(ap: TaskEither<E, Fn<A, B>>) =>
+	(ta: TaskEither<E, A>): TaskEither<E, B> => {
+		return async () => {
+			const [fn, a] = await Promise.all([ap(), ta()]);
+			return E.apply(fn)(a);
+		};
+	};
 
 export const applyw =
-  <E, A, B>(ap: TaskEither<E, Fn<A, B>>) =>
-  <E2>(ta: TaskEither<E2, A>): TaskEither<E | E2, B> => {
-    return async () => {
-      const [fn, a] = await Promise.all([ap(), ta()]);
-      return E.applyw(fn)(a);
-    };
-  };
+	<E, A, B>(ap: TaskEither<E, Fn<A, B>>) =>
+	<E2>(ta: TaskEither<E2, A>): TaskEither<E | E2, B> => {
+		return async () => {
+			const [fn, a] = await Promise.all([ap(), ta()]);
+			return E.applyw(fn)(a);
+		};
+	};
 
 export const sapply =
-  <E, A, B>(ap: TaskEither<E, Fn<A, B>>) =>
-  (ta: TaskEither<E, A>): TaskEither<E, B> => {
-    return pipe(
-      ap,
-      chain((fn) => pipe(ta, map(fn)))
-    );
-  };
+	<E, A, B>(ap: TaskEither<E, Fn<A, B>>) =>
+	(ta: TaskEither<E, A>): TaskEither<E, B> => {
+		return pipe(
+			ap,
+			chain((fn) => pipe(ta, map(fn))),
+		);
+	};
 
 export const sapplyw =
-  <E2, A, B>(ap: TaskEither<E2, Fn<A, B>>) =>
-  <E>(ta: TaskEither<E, A>): TaskEither<E | E2, B> => {
-    return pipe(
-      ap,
-      chainw((fn) => pipe(ta, map(fn)))
-    );
-  };
+	<E2, A, B>(ap: TaskEither<E2, Fn<A, B>>) =>
+	<E>(ta: TaskEither<E, A>): TaskEither<E | E2, B> => {
+		return pipe(
+			ap,
+			chainw((fn) => pipe(ta, map(fn))),
+		);
+	};
 
 export const delay =
-  (ms: number) =>
-  <E, A>(tea: TaskEither<E, A>): TaskEither<E, A> => {
-    return async () => {
-      await new Promise((resolve) => setTimeout(resolve, ms));
-      return tea();
-    };
-  };
+	(ms: number) =>
+	<E, A>(tea: TaskEither<E, A>): TaskEither<E, A> => {
+		return async () => {
+			await new Promise((resolve) => setTimeout(resolve, ms));
+			return tea();
+		};
+	};
 
 type RetryOptions<E = unknown> = {
-  maxRetries: number;
-  delay?: number;
-  onError?: Fn<E, void>;
-  skipIf?: Fn<E, boolean>;
+	maxRetries: number;
+	delay?: number;
+	onError?: Fn<E, void>;
+	skipIf?: Fn<E, boolean>;
 };
 export const retry =
-  <E>(options: RetryOptions<E>, acc = 0) =>
-  <A>(ta: TaskEither<E, A>): TaskEither<E, A> => {
-    return pipe(
-      ta,
-      chainLeft((e) => {
-        if (options.skipIf && options.skipIf(e)) return left(e);
-        if (acc >= options.maxRetries) return left(e);
-        if (options.onError) options.onError(e);
-        return pipe(ta, options.delay ? delay(options.delay) : id(), retry(options, acc + 1));
-      })
-    );
-  };
+	<E>(options: RetryOptions<E>, acc = 0) =>
+	<A>(ta: TaskEither<E, A>): TaskEither<E, A> => {
+		return pipe(
+			ta,
+			chainLeft((e) => {
+				if (options.skipIf && options.skipIf(e)) return left(e);
+				if (acc >= options.maxRetries) return left(e);
+				if (options.onError) options.onError(e);
+				return pipe(ta, options.delay ? delay(options.delay) : id(), retry(options, acc + 1));
+			}),
+		);
+	};
 
 interface TEF extends Monad2<URI>, BiFunctor2<URI>, Delayable2<URI>, Applicative2<URI> {}
 
